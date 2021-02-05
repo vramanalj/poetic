67d084ebb40b2c525cd0a488d82f40e7e2486ff4
venkat <vramanalj@gmail.com>
2021-02-05 23:51:57 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,99 +1,51 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Platform,
-  TouchableHighlight,
   Animated,
-  Easing,
+  Easing
 } from 'react-native';
-import logo from './logo.png';
-import { Button, ThemeProvider } from 'react-native-elements';
 
-class App extends Component {
-  state = {
-    spinValue: new Animated.Value(0),
-  };
+export default function App() {
 
-  onClick = () => {
-    const wasRotated = this.state.spinValue._value === 1;
-    Animated.timing(this.state.spinValue, {
-      toValue: wasRotated ? 0 : 1,
-      duration: 250,
-      easing: Easing.linear,
-    }).start();
-  };
-
-  render() {
-    const spin = this.state.spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg'],
+    useEffect(() => {
+        console.log("ComponentDidMount() Called.");
+        // Animated.timing(
+        //     fadeAnim,
+        //     {
+        //       toValue: 1,
+        //       duration: 10000,
+        //     }
+        //   ).start();
     });
+    
 
     return (
-      <View style={styles.container}>
-        <ThemeProvider>
-          <Button title="Hey!" />
-        </ThemeProvider>
-        <Animated.Image
-          source={logo}
-          style={[styles.logo, {transform: [{rotate: spin}]}]}
-        />
-        <Text style={styles.title}>Create React Native Web App</Text>
-        <Text style={styles.text}>
-          Open up src/App.js to start working on your app!
-        </Text>
-        <Text style={styles.text}>
-          Changes you make will automatically reload.
-        </Text>
-        {Platform.OS !== 'web' && (
-          <Text style={styles.text}>
-            Shake your phone to open the developer menu.
-          </Text>
-        )}
-        <TouchableHighlight
-          onPress={this.onClick}
-          style={styles.button}
-          underlayColor={'#0A84D0'}>
-          <Text style={styles.buttonText}>Rotate Logo</Text>
-        </TouchableHighlight>
-      </View>
+        <>
+            <Animated.View style={styles.container}> 
+                <Text h1 style={styles.appName}>Poetic</Text>
+            </Animated.View> 
+        </>
     );
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#282c34',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 300,
-    height: 300,
-  },
-  title: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    borderRadius: 3,
-    padding: 20,
-    marginVertical: 10,
-    marginTop: 10,
-    backgroundColor: '#1B95E0',
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-});
 
-export default App;
+    container: {
+        flex: 1,
+        backgroundColor: '#FFE74C',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    appName: {
+        fontSize: 108,
+        fontFamily: "Dancing Script",
+        color: '#413B3B',
+        textShadowColor: '#413B3B',
+        textShadowOffset: { width: 0, height: 3 },
+        textShadowRadius: 6
+    }
+
+});
---
+++ src/components/alphabetSelector/alphabetSelector.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    TouchableHighlight,
+    Animated,
+    Easing,
+  } from 'react-native';
+
+export default function AlphabetSelector() {
+    return (
+        <View>
+        <Text style={styles.text}>
+            Test
+        </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: '#fff',
+      }
+    }
+)
